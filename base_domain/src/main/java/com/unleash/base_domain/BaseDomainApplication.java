9c5c7d814637d5af90f34e586d8df2be668d0cc8
adarsh6672 <adarshkeruvatty@gmail.com>
2024-05-27 18:40:06 +0530

    base domain for commonly used dtos for better management of kafka

---
+++ base_domain/src/main/java/com/unleash/base_domain/BaseDomainApplication.java
@@ -0,0 +1,13 @@
+package com.unleash.base_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDomainApplication.class, args);
+	}
+
+}
